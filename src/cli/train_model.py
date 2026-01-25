229435e4311e83c941147cbaf7c554d29bc9a609
Eugen Didenko <didenkojack@gmail.com>
2026-01-25 10:29:02 +0300

    add normalization to model

--- src/cli/train_model.py
+++ src/cli/train_model.py
@@ -3,6 +3,33 @@ from omegaconf import DictConfig
 import pytorch_lightning as L
 from hydra.utils import instantiate
 from ..models.jepa import JepaLight
+from ..data_utils.datamodule import GraphDataModule, GraphDataSet
+from ..data_utils.normalization import GenNormalize
+import torch
+
+def load_stats(path):
+    mean_x = torch.load(path+"means.pt")
+    std_x = torch.load(path+"stds.pt")
+    mean_edge = torch.load(path+"mean_edge.pt")
+    std_edge = torch.load(path+"std_edge.pt")
+    return mean_x,std_x, mean_edge,std_edge
+
+def get_datamodule(cfg):
+    mean_x,std_x, mean_edge,std_edge = load_stats('/home/eugen/Desktop/CodeWork/Projects/Diplom/notebooks/GIT_Graph_refactor/data/stats/')
+    transfrom = GenNormalize(mean_x,std_x, mean_edge,std_edge)
+
+
+    ds = GraphDataSet(path = cfg.dataset.path,transform=transfrom)
+
+    datamodule = GraphDataModule(ds, cfg.batch_size,
+                                 num_workers=cfg.num_workers, 
+                                 seed = cfg.seed,
+                                 ratio = cfg.ratio,
+
+                                 )
+    return datamodule
+
+
 @hydra.main(version_base="1.3", config_path="../../configs", config_name="config")
 def main(cfg: DictConfig):
     L.seed_everything(cfg.seed, workers=True)
@@ -21,7 +48,7 @@ def main(cfg: DictConfig):
         #callbacks=[checkpoint_callback]
     )
 
-    datamodule = instantiate(cfg.datamodule, _recursive_=True)
+    datamodule = get_datamodule(cfg.datamodule)
     trainer.fit(model_module, datamodule=datamodule)
 
 if __name__ == "__main__":
